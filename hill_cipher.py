1628e01b951f3cebda44db95a817c3b2edf0de63
saoalo <sampansnayak2@gmail.com>
2020-11-20 16:42:46 +0530

    extended hill cipher functionality

--- hill_cipher.py
+++ hill_cipher.py
@@ -1,7 +1,31 @@
 import numpy as np
 import math
 
-def encrypt(plain_text,key_matrix,debug=False):
+def encrypt(plain_text,key_matrix):
+    plain_text_words = plain_text.split(' ')
+    cipher_text = []
+    extras  = []
+
+    for word in plain_text_words:
+        cipher_text_word,extra = encrypt_word(word,key_matrix)
+        cipher_text.append(cipher_text_word)
+        extras.append(extra)
+    
+    return " ".join(cipher_text),extras
+
+def decrypt(cipher_text,key_matrix,extras=None):
+    cipher_text_words = cipher_text.split(' ')
+
+    if(extras == None):
+        extras = [0 for i in range(len(cipher_text_words))]
+
+    plain_text = []
+    for i in range(len(cipher_text_words)):
+        plain_text.append(decrypt_word(cipher_text_words[i],key_matrix,extras[i]))
+    
+    return " ".join(plain_text)
+
+def encrypt_word(plain_text,key_matrix,debug=False):
     key_matrix = np.array(key_matrix)
     key_dim = key_matrix.shape[0]
     dimensions = len(plain_text)
@@ -45,7 +69,7 @@ def encrypt(plain_text,key_matrix,debug=False):
     return (cipher_text,extra)
 
 # decryption function
-def decrypt(cipher_text,key_matrix,extra=0,debug=False):
+def decrypt_word(cipher_text,key_matrix,extra=0,debug=False):
     key_matrix = np.array(key_matrix)
     key_dim = key_matrix.shape[0]
     key_matrix_inv = np.linalg.inv(key_matrix)
@@ -83,14 +107,38 @@ def decrypt(cipher_text,key_matrix,extra=0,debug=False):
 
     return plain_text
 
-def check_whether_invertible(matrix):
-    pass
+def check_key(key_matrix):
+    key_matrix = np.array(key_matrix)
+    if(key_matrix.shape[0] != key_matrix.shape[1]):
+        return False
+
+    det = np.linalg.det(key_matrix)
+
+    if(det == 0 or det%2 == 0 or det%13 == 0):
+        return False
+
+    test_string = "TEST"
+    cipher_text,extras = encrypt(test_string,key_matrix)
+    plain_text = decrypt(cipher_text,key_matrix,extras)
+
+    if(test_string != plain_text):
+        return False
+    
+    return True
 
 if __name__ == "__main__":
-    plain_text = str(input("Plain Text: "))
+    # plain_text = str(input("Plain Text: "))
     key_matrix = np.array([[8,6,9,5],[6,9,5,10],[5,8,4,9],[10,6,11,4]])
-    # key_matrix = np.array([[1,1,1,-1],[1,1,-1,1],[1,-1,1,1],[-1,1,1,1]])
+    key_matrix2 = np.array([[1,1,1,-1],[1,1,-1,1],[1,-1,1,1],[-1,1,1,1]])
+    key_matrix3 = np.array([[2,5,0,8],[1,4,2,6],[7,8,9,3],[1,5,7,8]])
 
-    cipher_text,extra = encrypt(plain_text,key_matrix,debug=True)
+    print(check_key(key_matrix))
+    print(check_key(key_matrix2))
+    print(check_key(key_matrix3))
 
-    plain_text = decrypt(cipher_text,key_matrix,extra,debug=True)
+    # cipher_text,extra = encrypt(plain_text,key_matrix)
+    # print("cipher text: ",cipher_text)
+
+    # plain_text = decrypt(cipher_text,key_matrix,extra)
+    # print("plain text: ",plain_text)
+
